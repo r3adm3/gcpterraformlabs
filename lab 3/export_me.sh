57cc9e821ef49fd449c173f51cf9deee72664c87
Bakh Inamov <shipit@google.com>
2018-09-16 16:37:36 -0700

    fixes kitchen veirfy

--- lab 3/export_me.sh
+++
@@ -1,8 +0,0 @@
-
-echo "Go to https://console.cloud.google.com/home/dashboard"
-
-export GCP_PROJECT_NAME=<project-name>
-export GCP_PROJECT_NUMBER=<project-number>
-export GCP_PROJECT_ID=<project-id>
-export GCP_LOCATION=<region, defaults to europe-west2>
-export GCP_ZONE=<zone, defaults to europe-west2-a>
