2fbacf3be5893bcef650e76e7fb9173d0a1adca4
Bakh Inamov <shipit@google.com>
2018-09-16 12:52:52 -0700

    should_not allow 21

--- lab 2/tests/controls/firewalls.rb
+++ lab 2/tests/controls/firewalls.rb
@@ -3,9 +3,9 @@ gcp_project = ENV["PROJECT"]
 control 'gcp-firewalls-no-ftp' do
   title 'Ensure no FTP allowed in firewall rules'
   google_compute_firewalls(project: gcp_project).firewall_names.each do |firewall_name|
-    describe google_compute_firewall(project: gcp_project, name: firewall_name) do
+    describe google_compute_firewall(project: gcp_project, name: firewall_name, source_ranges: ["0.0.0.0/0"]) do
       it { should exist }
-      its('allow_port_protocol(port: "21", protocol: "tcp")?')  { should be false }
+      it { should_not allow_port_protocol("21", "tcp")}
     end
   end
 end
