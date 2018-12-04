d61c90b13b878b1280aff11eee47786dca03b7c0
Bakh Inamov <shipit@google.com>
2018-12-04 11:50:56 -0800

    tf 11.10 / kt 4.0.6

--- setup.sh
+++ setup.sh
@@ -15,14 +15,14 @@ cd $HOME
 mkdir terraform11
 cd terraform11
 sudo apt-get install unzip
-wget https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
-unzip terraform_0.11.7_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/0.11.10/terraform_0.11.10_linux_amd64.zip
+unzip terraform_0.11.10_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
 cd $HOME
 rm -rf terraform11
 
 # Install kitchen-terraform with its dependenncies like inspec and test-kitchen
-gem install kitchen-terraform  -v 4.0.1 --no-ri --no-rdoc
+gem install kitchen-terraform  -v 4.0.6 --no-ri --no-rdoc
 
 # # Get email for the GCE default service account
 export GCE_EMAIL=$(gcloud iam service-accounts list --format='value(email)' | grep compute)
