7082c47ce688a51883410ac8c9d98c360ebc17c1
Ben Bleything <ben@bleything.net>
2019-02-21 23:17:33 +0000

    minor tweaks and version updates

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+TERRAFORM_VERSION=0.11.11
+KITCHEN_TERRAFORM_VERSION=4.3.0
+
 # Create gems path
 cd $HOME
 mkdir -p .gems/bin
@@ -9,22 +12,21 @@ PATH=$PATH:$HOME/.gems/bin
 mkdir .ssh
 ssh-keygen -t rsa -b 4096 -a 100 -N "" -f .ssh/gcloud_id_rsa
 
-
 # Install terraform
 cd $HOME
 mkdir terraform11
 cd terraform11
 sudo apt-get install unzip
-wget https://releases.hashicorp.com/terraform/0.11.10/terraform_0.11.10_linux_amd64.zip
-unzip terraform_0.11.10_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
 cd $HOME
 rm -rf terraform11
 
-# Install kitchen-terraform with its dependenncies like inspec and test-kitchen
-gem install kitchen-terraform  -v 4.0.6 --no-ri --no-rdoc
+# Install kitchen-terraform with its dependencies like inspec and test-kitchen
+gem install kitchen-terraform -v $KITCHEN_TERRAFORM_VERSION --no-document
 
-# # Get email for the GCE default service account
+# Get email for the GCE default service account
 export GCE_EMAIL=$(gcloud iam service-accounts list --format='value(email)' | grep compute)
 
 # Get creds for default CE SA
