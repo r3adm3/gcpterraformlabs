134b6bf9f16689a44af6ad9caaeaf2da430392e5
Adrian Farnell <adrianjfarnell@gmail.com>
2019-08-15 12:09:59 +0100

    added skip for default fw

--- lab 2/gcptests/controls/firewalls.rb
+++ lab 2/gcptests/controls/firewalls.rb
@@ -2,6 +2,7 @@ gcp_project = ENV["GOOGLE_CLOUD_PROJECT"]
 control 'gcp-firewalls-no-ftp' do
   title 'Ensure no FTP allowed in firewall rules'
   google_compute_firewalls(project: gcp_project).firewall_names.each do |firewall_name|
+    next if firewall_name == "default-allow-internal"
     describe google_compute_firewall(project: gcp_project, name: firewall_name) do
       it { should_not allow_port_protocol("21", "tcp")}
     end
